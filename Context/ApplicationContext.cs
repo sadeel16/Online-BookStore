f12d1c184f26033ed9184a39ffc95855eef150b3
Sadeel Malassa <30291492+sadeel16@users.noreply.github.com>
2023-05-21 16:53:16 +0300

    Add files via upload

---
+++ Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using BookStore.Models;
+
+namespace WebApplication2.Context
+{
+    public class ApplicationContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cart_Item> Carts { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Shoping_Session> shoping_Sessions { get; set; }
+        
+    }
+}
